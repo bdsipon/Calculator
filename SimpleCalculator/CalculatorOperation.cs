16d9a0594dfc35a772115a3c5168a4a409cd7e6f
Sipon Hossain <sipon.here@gmail.com>
2017-10-09 15:43:58 +1000

    Add project files.

---
+++ SimpleCalculator/CalculatorOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorOperation
+    {
+        public double calculate(string argOperation,double argFirstNumber,double argSecondNumber)
+        {
+            double result;
+            switch(argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+
+                case "substract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specification is not recognised");
+                   
+            }
+            return result;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputData inputData = new InputData();
+                CalculatorOperation calculatorOperation = new CalculatorOperation();
+
+                double firstNumber, secondNumber;
+                string operation;
+                firstNumber = inputData.ConvertDatatoNumberic(Console.ReadLine());
+                secondNumber = inputData.ConvertDatatoNumberic(Console.ReadLine());
+                operation = Console.ReadLine();
+
+                double result = calculatorOperation.calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine(result);
+            
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
